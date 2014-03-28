d768babfebedd09e3988235cdf2d87c052059b93
sergiocormio <sergiocormio@459b3859-416c-13d8-66aa-c34694e86061>
2014-03-28 04:23:48 +0000

    HighlightingPersistor first implementation. Highlighting improved.

--- trunk/DumpAnalyzer/src/resources/ResourcesFactory.java
+++ trunk/DumpAnalyzer/src/resources/ResourcesFactory.java
@@ -4,10 +4,13 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 public class ResourcesFactory {
+	
+		//APP icon
 		public static ImageIcon getDumpIcon(){
 			return new ImageIcon(ResourcesFactory.class.getResource("rigid_dump_truck.png"));
 		}
 		
+		//Open icon
 		public static Icon getOpenIcon(){
 			return new ImageIcon(ResourcesFactory.class.getResource("open-file.png"));
 		}
@@ -15,5 +18,10 @@ public class ResourcesFactory {
 		//APP Image
 		public static Icon getAppImage() {
 			return new ImageIcon(ResourcesFactory.class.getResource("rigid_dump_truck 256.png"));
+		}
+		
+		//Highlighting
+		public static Icon getHighlightingIcon() {
+			return new ImageIcon(ResourcesFactory.class.getResource("highlighter-text.png"));
 		}
 }
